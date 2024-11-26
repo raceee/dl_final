604e2e813f5e366bd6f9beb5f9d229aa4424d45d
Michael Sullivan <72317477+sully1102@users.noreply.github.com>
2024-11-26 11:13:05 -0700

    Iteration 1 of Dataset, Model, and main script

--- graph/model.py
+++ graph/model.py
@@ -1,21 +1,26 @@
 import torch
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv, global_mean_pool
 
-class GNN:
-    def __init__(self, num_layers, num_features, num_classes):
-        self.num_layers = num_layers
-        self.num_features = num_features
-        self.num_classes = num_classes
-        self.layers = nn.ModuleList()
-        self.layers.append(GCNConv(num_features, 16))
-        for i in range(num_layers - 1):
-            self.layers.append(GCNConv(16, 16))
-        self.layers.append(GCNConv(16, num_classes))
-        self.dropout = nn.Dropout(0.5)
+# Define the Graph Neural Network model
+class GraphNN_Model(torch.nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(GraphNN_Model, self).__init__()
+        self.conv1 = GCNConv(input_dim, hidden_dim)
+        self.conv2 = GCNConv(hidden_dim, hidden_dim)
+        self.fc = torch.nn.Linear(hidden_dim, output_dim)
 
-    def forward(self, x, edge_index):
-        for i in range(self.num_layers):
-            x = self.layers[i](x, edge_index)
-            x = F.relu(x)
-            x = self.dropout(x)
-        x = self.layers[self.num_layers](x, edge_index)
-        return
+    def forward(self, data):
+        x, edge_index, batch = data.x, data.edge_index, data.batch
+        x = x.float()
+
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
+        x = self.conv2(x, edge_index)
+        x = F.relu(x)
+
+        x = global_mean_pool(x, batch)
+
+        x = self.fc(x)
+        
+        return x
--- train_graph.py
+++ train_graph.py
@@ -1,21 +1,46 @@
-import os
+import torch
 import pandas as pd
+from torch_geometric.loader import DataLoader
+
 from util.training_set import GetTrainingSet
-from graph.model import GraphModel
-from graph.dataset import Dataset
-from torch.utils.data import DataLoader
+from graph.dataset import GraphNN_Dataset
+from graph.model import GraphNN_Model
 
-training_df, last_unique_df, random_in_training_df = GetTrainingSet("data/train_sample.csv")
 
-training_dataset = Dataset(training_df)
-last_unique_dataset = Dataset(last_unique_df)
-random_in_training_dataset = Dataset(random_in_training_df)
+    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # print(f"Using device: {device}")
 
-training_dataloader = DataLoader(training_dataset, batch_size=32, shuffle=True)
-last_unique_dataloader = DataLoader(last_unique_dataset, batch_size=32, shuffle=True)
-random_in_training_dataloader = DataLoader(random_in_training_dataset, batch_size=32, shuffle=True)
+    
 
-for i in hypers:
-    model = GraphModel(num_layers=i["num_layers"], num_features=i["num_features"], num_classes=i["num_classes"])
-    model.train(training_df, last_unique_df, random_in_training_df, i["epochs"], i["batch_size"], i["learning_rate"], i["weight_decay"], i["device"])
-    model.save_model(f"model_{i['num_layers']}_{i['num_features']}_{i['num_classes']}")
+
+if __name__ == '__main__':
+
+    path = "data/train_sample.csv"
+    training_df, last_unique_df, random_in_training_df = GetTrainingSet(path).get_training_data()
+
+    train_size = int(0.8 * len(training_df))
+    train_df = training_df.iloc[:train_size]
+    val_df = training_df.iloc[train_size:]
+
+    column_name = 'molecule_smiles'
+    train_smiles_list = train_df[column_name].tolist()
+    val_smiles_list = val_df[column_name].tolist()
+
+    train_smiles_list = train_df['molecule_smiles'].tolist()
+    val_smiles_list = val_df['molecule_smiles'].tolist()
+
+    train_dataset = GraphNN_Dataset(train_smiles_list)
+    val_dataset = GraphNN_Dataset(val_smiles_list)
+    
+    train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+    val_dataloader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+
+    graph_data = train_dataset[0].edge_index
+    print(graph_data)
+
+    # input_dim = 1
+    # hidden_dim = 128
+    # output_dim = 1
+    # model = GraphNN_Model(input_dim, hidden_dim, output_dim)
+
+    
