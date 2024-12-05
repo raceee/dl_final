d84149e0c11fb3710c8144ea7c70f5fb930c5731
raceee <race.peterson@gmail.com>
2024-12-04 21:54:04 -0700

    #10 silhouette scores

--- train_transformer.py
+++ train_transformer.py
@@ -21,9 +21,7 @@ else:
     device = torch.device("cpu")
 
 
-# Main function
 def main():
-    # Load and prepare data
     training_df, _, _ = GetTrainingSet("data/train_sample.csv").get_training_data()
     training_df = training_df.sample(frac=1).reset_index(drop=True)
 
@@ -36,14 +34,12 @@ def main():
 
     tokenizer = AutoTokenizer.from_pretrained('DeepChem/ChemBERTa-77M-MTR')
 
-    # Datasets and DataLoaders
     train_dataset = MoleculeDataset(train_smiles_list, tokenizer)
     val_dataset = MoleculeDataset(val_smiles_list, tokenizer)
 
     train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
     val_dataloader = DataLoader(val_dataset, batch_size=32, shuffle=False)
 
-    # Initialize model, optimizer, and scheduler
     model = MoleculeBERTModel(vocab_size=tokenizer.vocab_size).to(device)
     optimizer = torch.optim.AdamW(model.parameters(), lr=5e-5)
     total_steps = len(train_dataloader) * 10  # Assuming 10 epochs
@@ -54,7 +50,6 @@ def main():
     )
     loss_fn = nn.CrossEntropyLoss()
 
-    # Initialize and run Trainer
     trainer = Trainer(
         model=model,
         train_dataloader=train_dataloader,
@@ -65,20 +60,17 @@ def main():
         criterion=loss_fn
     )
 
-    # Train the model
     history = trainer.train(num_epochs=10, save_best_model_path=f"transformer_checkpoints/num_hidden_layers_{model.num_hidden_layers}.pth")
 
-    # Plot and save the loss curves
     trainer.plot_loss_curves(
         history=history,
         model_name="MoleculeBERT with AdamW",
-        save_path="plots",  # Directory to save the plot
-        show_plot=True      # Set to True to display the plot
+        save_path="plots",
+        show_plot=True
     )
     
     trainer.infer_clusters("./data/last_unique_smiles.csv", tokenizer, method="umap")
-    
 
-# Entry point
+
 if __name__ == "__main__":
     main()
