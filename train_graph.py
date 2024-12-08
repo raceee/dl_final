c30fbb3d92c0ddb36b72100304a8b8ea8442e265
Michael Sullivan <72317477+sully1102@users.noreply.github.com>
2024-12-08 13:38:30 -0700

    train_graph now does grid search

--- train_graph.py
+++ train_graph.py
@@ -1,12 +1,15 @@
+import json
 import math
 import torch
 import pandas as pd
+from datetime import datetime
 from torch_geometric.loader import DataLoader
 from torch.optim.lr_scheduler import StepLR
 
 from util.training_set import GetTrainingSet
 from graph.dataset import GraphNN_Dataset
 from graph.model import GraphNN_Model
+from graph.model_wo import GraphNN_Model_WO
 from util.trainer_gnn import Trainer_GNN
 
 # Determine the device
@@ -31,11 +34,13 @@ def main():
     output_dim =  118
 
     # Interesting to grid search on
-    hidden_dim = 64 # 32, 64, 128, 256
-    num_hidden_layers = 2 # 2, 3, 4
+    hidden_dims = [32, 64, 128, 256]
+    num_hidden_layerss = [2, 3, 4]
 
     # Load the training data
-    training_df, _, _ = GetTrainingSet("data/train_sample.csv").get_training_data()
+    # data_path = "data/train_sample.csv"
+    data_path = "data/train.csv"
+    training_df, last_unique_df, random_unique_df = GetTrainingSet(data_path).get_training_data()
 
     train_size = int(0.8 * len(training_df))
     train_df = training_df.iloc[:train_size]
@@ -52,49 +57,61 @@ def main():
     val_dataloader = DataLoader(val_dataset, batch_size=32, shuffle=False)
 
     # Grid search loop here
-    
-    # Initialize model, optimizer, and scheduler
-    model = GraphNN_Model(input_dim, hidden_dim, output_dim, num_hidden_layers=num_hidden_layers).to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    hp_results = {}
+    for hidden_dim in hidden_dims:
+        for num_hidden_layers in num_hidden_layerss:
+            print(f"Training model with hidden_dim={hidden_dim} and num_hidden_layers={num_hidden_layers}")
+            
+
+            # Initialize model, optimizer, and scheduler
+            # model = GraphNN_Model(input_dim, hidden_dim, output_dim, num_hidden_layers=num_hidden_layers).to(device)
+            model = GraphNN_Model_WO(input_dim, hidden_dim, output_dim, num_hidden_layers=num_hidden_layers).to(device)
+            optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 
-    # Calculate the step size for the scheduler
-    num_epochs_per_decay = 30
-    batches_per_epoch = math.ceil(len(train_dataloader) / batch_size)
-    step_size = num_epochs_per_decay * batches_per_epoch
+            # Calculate the step size for the scheduler
+            num_epochs_per_decay = 30
+            batches_per_epoch = math.ceil(len(train_dataloader) / batch_size)
+            step_size = num_epochs_per_decay * batches_per_epoch
 
-    scheduler = StepLR(optimizer,
-                       step_size=step_size,
-                       gamma=gamma)
-    criterion = torch.nn.CrossEntropyLoss()
+            scheduler = StepLR(optimizer,
+                            step_size=step_size,
+                            gamma=gamma)
+            criterion = torch.nn.CrossEntropyLoss()
 
-    # Initialize and run Trainer
-    trainer = Trainer_GNN(
-        model=model,
-        train_dataloader=train_dataloader,
-        val_dataloader=val_dataloader,
-        optimizer=optimizer,
-        scheduler=scheduler,
-        device=device,
-        criterion=criterion
-    )
-   
-    # Train the model
-    history = trainer.train(num_epochs=num_epochs,
-                            smiles_df_path="data/last_unique_smiles.csv",
-                            save_best_model_path=f"gnn_checkpoints/hidden_dim_{hidden_dim}.pth")
-    
-    # Plot and save the loss curves
-    trainer.plot_loss_curves(
-        history=history,
-        model_name="GraphNN with Adam",
-        save_path="plots",
-        show_plot=False
-    )
+            # Initialize and run Trainer
+            trainer = Trainer_GNN(
+                model=model,
+                train_dataloader=train_dataloader,
+                val_dataloader=val_dataloader,
+                optimizer=optimizer,
+                scheduler=scheduler,
+                device=device,
+                criterion=criterion
+            )
+        
+            # Train the model
+            history = trainer.train(num_epochs=num_epochs,
+                                    smiles_df_path="data/last_unique_smiles.csv",
+                                    save_best_model_path=f"gnn_checkpoints/hidden_dim_{hidden_dim}.pth")
+            
+            # Plot and save the loss curves
+            # trainer.plot_loss_curves(
+            #     history=history,
+            #     model_name="GraphNN with Adam",
+            #     save_path="plots",
+            #     show_plot=False
+            # )
 
-    ### Doesn't throw errors until here
-    trainer.infer_clusters("data/last_unique_smiles.csv", method="umap")
+            # trainer.infer_clusters("data/last_unique_smiles.csv", method="umap", show_plot=False)
 
+            # Save the results
+            hp_results[f"{hidden_dim}, {num_hidden_layers}"] = history
+
+    # Save the results to a JSON file
+    now = datetime.now().strftime("%y%m_%d%H")
+    with open(f"gs_results/{now}.json", "w") as f:
+        json.dump(hp_results, f)
 
 if __name__ == '__main__':
     main()
